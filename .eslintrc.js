85495538d382da676cb76e9c80272022983ee6a7
y.hoshi <ysk.hoshi.1118@gmail.com>
2020-06-27 01:36:18 +0900

    add: format & config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: ['@nuxtjs/eslint-config-typescript', 'plugin:vue/essential'],
+    plugins: ['vue'],
+    // add your custom rules here
+    rules: {
+        'vue/name-property-casing': ['error', 'kebab-case'],
+        'vue/html-indent': ['error', 4],
+        indent: ['error', 4],
+        'space-before-function-paren': ['error', 'never']
+    }
 }
