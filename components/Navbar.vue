4fdf13b1218579801c35f5e3bb136cf477255cad
y.hoshi <ysk.hoshi.1118@gmail.com>
2020-06-28 23:30:27 +0900

    fix: Aboutのテンプレートを修正

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,18 +1,38 @@
 <template>
     <nav class="gnav fixed">
         <ul class="gnav-list">
-            <li class="gnav-item">
-                <a class="gnav-link" href="#home-section">HOME</a>
-            </li>
-            <li class="gnav-item">
-                <a class="gnav-link" href="#about-section">ABOUT</a>
-            </li>
-            <li class="gnav-item">
-                <a class="gnav-link" href="#skill-section">SKILL</a>
-            </li>
-            <li class="gnav-item">
-                <a class="gnav-link" href="#contact-section">CONTACT</a>
+            <li v-for="(item, key) in columns" :key="key" class="gnav-item">
+                <a class="gnav-link" :href="item.ref">{{ item.text }}</a>
             </li>
         </ul>
     </nav>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    name: 'navbar',
+    data() {
+        return {
+            columns: {
+                home: {
+                    ref: '#home-section',
+                    text: 'HOME'
+                },
+                about: {
+                    ref: '#about-section',
+                    text: 'ABOUT'
+                },
+                skill: {
+                    ref: '#skill-section',
+                    text: 'SKILL'
+                },
+                contact: {
+                    ref: '#contact-section',
+                    text: 'CONTACT'
+                }
+            }
+        }
+    }
+})
+</script>
